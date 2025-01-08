bc4b726f13eadd873b7ba32637970d7397ea6e73
Caleb <50220431+ohenecaleb@users.noreply.github.com>
2025-01-08 17:06:34 -0500

    Update Day2 Variables and DataTypes.py

--- Day2 Variables and DataTypes.py
+++ Day2 Variables and DataTypes.py
@@ -1,3 +1,8 @@
+#Data Types and Variables
+
 #Creating a variable to accept name and age from users
 name = input("Enter your name: ")
 age = input("Enter your age: ")
+DOB = 2021 - int(age)
+#Printing the name and age of the user
+print("Hello " + name + "! Your date of birth is " + str(DOB) )
