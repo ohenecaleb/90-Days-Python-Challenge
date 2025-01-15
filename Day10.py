759903aa76ac91e3dab4ef1a0f1355d1e506b620
Caleb <50220431+ohenecaleb@users.noreply.github.com>
2025-01-15 22:34:11 +0000

    Add files via upload

---
+++ Day10.py
@@ -0,0 +1,13 @@
+import math
+
+def calculate_square_root(number):
+    if number < 0:
+        raise ValueError("Cannot calculate the square root of a negative number.")
+    return math.sqrt(number)
+
+number = float(input("Enter a number: "))
+try:
+    result = calculate_square_root(number)
+    print(f"The square root of {number} is {result}")
+except ValueError as e:
+    print(e)
