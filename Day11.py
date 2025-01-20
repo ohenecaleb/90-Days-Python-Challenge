7a8e369839aef8de33d378de98cebf1dfde39f7f
Caleb <50220431+ohenecaleb@users.noreply.github.com>
2025-01-20 18:21:38 -0500

    Day11.py RegEx

--- Day11.py
+++ Day11.py
@@ -10,3 +10,22 @@ def is_valid_email(email):
 emails = ["test@example.com", "invalid-email@", "user.name@domain.co", "user@domain"]
 for email in emails:
     print(f"{email}: {'Valid' if is_valid_email(email) else 'Invalid'}")
+
+#Explanation line 6 below
+"""
+[a-zA-Z0-9-]: This part of the pattern defines a character class that matches any single character that is:
+
+A lowercase letter (a-z)
+An uppercase letter (A-Z)
+A digit (0-9)
+A hyphen (-)
++: This quantifier means "one or more" of the preceding character class. 
+It ensures that the pattern matches one or more characters from the character 
+class [a-zA-Z0-9-].
+
+\.: This matches a literal dot (.). The backslash (\) is used to escape the 
+dot because, in regular expressions, a dot normally matches any character.
+ Escaping it with a backslash makes it match only a literal dot.
+
+ Code By Ohene Caleb
+"""
